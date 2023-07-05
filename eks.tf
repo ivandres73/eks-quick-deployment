b965817972caca4c2ac68f558886528b7f0c0a32
Ivan C <ivandres73@unitec.edu>
2023-07-05 16:39:28 -0600

    adding node_group resource (terraform apply works at this point)

--- eks.tf
+++ eks.tf
@@ -21,8 +21,7 @@ resource "aws_eks_cluster" "eks_cluster" {
   # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
   # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
   depends_on = [
-    aws_iam_role_policy_attachment.eks_cluster_role_attach,
-    //aws_iam_role_policy_attachment.example-AmazonEKSVPCResourceController,
+    aws_iam_role_policy_attachment.eks_cluster_role_attach
   ]
 }
 
@@ -32,14 +31,37 @@ resource "aws_eks_addon" "vpc_cni_addon" {
   addon_version = "v1.12.6-eksbuild.2"
 }
 
-resource "aws_eks_addon" "coredns_addon" {
-  cluster_name = aws_eks_cluster.eks_cluster.name
-  addon_name   = "coredns"
-  addon_version = "v1.10.1-eksbuild.1"
-}
+/// must deploy after having node groups
+# resource "aws_eks_addon" "coredns_addon" {
+#   cluster_name = aws_eks_cluster.eks_cluster.name
+#   addon_name   = "coredns"
+#   addon_version = "v1.10.1-eksbuild.1"
+# }
 
-resource "aws_eks_addon" "kube_proxy_addon" {
-  cluster_name = aws_eks_cluster.eks_cluster.name
-  addon_name   = "kube-proxy"
-  addon_version = "v1.27.1-eksbuild12"
+# resource "aws_eks_addon" "kube_proxy_addon" {
+#   cluster_name = aws_eks_cluster.eks_cluster.name
+#   addon_name   = "kube-proxy"
+#   addon_version = "v1.27.1-eksbuild12"
+# }
+
+resource "aws_eks_node_group" "eks_node_role" {
+  cluster_name    = aws_eks_cluster.eks_cluster.name
+  node_group_name = "group1"
+  node_role_arn   = aws_iam_role.eks_node_role.arn
+  subnet_ids      = ["subnet-e866b3a5", "subnet-5f767703"]
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 1
+    min_size     = 1
+  }
+
+  capacity_type = "SPOT"
+  instance_types = ["t3.micro"]
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
+  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
+  depends_on = [
+    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy_attach
+  ]
 }
--- iam.tf
+++ iam.tf
@@ -1,3 +1,5 @@
+//////  CLUSTER PERMISSIONS
+
 resource "aws_iam_role" "eks_cluster_role" {
   name = "eksClusterRole"
   description = "Amazon EKS - Cluster role"
@@ -28,4 +30,39 @@ data "aws_iam_policy" "AmazonEKSClusterPolicy" {
 resource "aws_iam_role_policy_attachment" "eks_cluster_role_attach" {
   role       = aws_iam_role.eks_cluster_role.name
   policy_arn = data.aws_iam_policy.AmazonEKSClusterPolicy.arn
+}
+
+////// NODES PERMISSIONS
+
+resource "aws_iam_role" "eks_node_role" {
+  name = "eks_node_role"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+// allows access to EKS info
+resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy_attach" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+// allows IP change of the EC2
+resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy_attach" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+// allows pulling images from AWS-ECR
+resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly_attach" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_node_role.name
 }
