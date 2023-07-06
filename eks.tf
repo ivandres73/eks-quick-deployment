ca20afedc1b016c98edaad871962ff54a13b839a
Ivan C <ivandres73@unitec.edu>
2023-07-05 23:47:55 -0600

    commenting spot attribute (it made a pod undeployable)

--- eks.tf
+++ eks.tf
@@ -44,7 +44,7 @@ resource "aws_eks_addon" "vpc_cni_addon" {
 #   addon_version = "v1.27.1-eksbuild12"
 # }
 
-resource "aws_eks_node_group" "eks_node_role" {
+resource "aws_eks_node_group" "eks_node_group" {
   cluster_name    = aws_eks_cluster.eks_cluster.name
   node_group_name = "group1"
   node_role_arn   = aws_iam_role.eks_node_role.arn
@@ -56,8 +56,8 @@ resource "aws_eks_node_group" "eks_node_role" {
     min_size     = 1
   }
 
-  capacity_type = "SPOT"
-  instance_types = ["t3.micro"]
+  # capacity_type = "SPOT" // this line dont let the node to join the node-group
+  instance_types = ["t3.small"]
 
   # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
   # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
